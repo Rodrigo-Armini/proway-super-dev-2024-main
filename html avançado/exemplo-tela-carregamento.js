37094166818faf9caf51670c8a9f78b7f64495c9
Rodrigo Armini <rodrigoarmini@gmail.com>
2025-02-14 19:10:56 -0300

    Delete html avançado directory

--- html avançado/exemplo-tela-carregamento.js
+++
@@ -1,9 +0,0 @@
-function terminarCarregament(){
-    let divCarregando = document.getElementById("loading");
-    divCarregando.style.display = "none";
-    
-    let main = document.getElementsByTagName("main")[0];
-    main.style.display = "block";
-}
-
-window.addEventListener("load", terminarCarregament);
